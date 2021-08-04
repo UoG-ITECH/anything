9edc67abf949aa583b54ce73df2d9528fdf461d8
ramon <2593406m@student.gla.ac.uk>
2021-08-04 12:46:10 +0100

    Added Buy button to product screen Started filling out populate_rango.py

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -1,4 +1,5 @@
 import os
+import pickle
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'tango_with_django_project.settings')
@@ -25,32 +26,31 @@ def populate():
 
     business = [
         {'name': 'Asus 2',
-         'url': 'http://docs.python.org/3/tutorial/',
          'description': desc,
          'price': 600.50},
         {'name': 'Asus 3',
-         'url': 'http://www.greenteapress.com/thinkpython/',
          'description': desc,
          'price': 560.50,
          },
         {'name': 'Asus 5',
-         'url': 'http://www.korokithakis.net/tutorials/python/',
          'description': desc,
          'price': 480.50
-         }]
+         },
+        {'name': 'Asus 15',
+         'description': desc,
+         'price': 854.50
+         }
+    ]
 
     portable = [
         {'name': 'Asus 6',
-         'url': 'http://docs.python.org/3/tutorial/',
          'description': desc,
          'price': 800.50},
         {'name': 'Asus 7',
-         'url': 'http://www.greenteapress.com/thinkpython/',
          'description': desc,
          'price': 600.50
          },
         {'name': 'Asus 8',
-         'url': 'http://www.korokithakis.net/tutorials/python/',
          'description': desc,
          'price': 300.50
          }
@@ -58,17 +58,14 @@ def populate():
 
     gaming = [
         {'name': 'Asus 9',
-         'url': 'http://docs.python.org/3/tutorial/',
          'description': desc,
          'price': 1600.50
          },
         {'name': 'Asus 10',
-         'url': 'http://www.greenteapress.com/thinkpython/',
          'description': desc,
          'price': 1000.50
          },
         {'name': 'Asus 11',
-         'url': 'http://www.korokithakis.net/tutorials/python/',
          'description': desc,
          'price': 800.50
          }]
@@ -77,30 +74,57 @@ def populate():
             'Business': {'pages': business},
             'Portable': {'pages': portable}}
 
+    reviews= [
+        {'user': 'Kevin89',
+         'product': "Asus 15",
+         'rating' : 10,
+         'content': 'Thought it was an excellent machine! Works great for me.',
+         'date': 'Jul. 11, 2021, 08:31 p.m.'},
+        {'user': 'Kevin78',
+         'product': "Asus 15",
+         'rating': 8,
+         'content': 'Thought it was a terrible machine! Works badly for me.',
+         'date': 'Jun. 15, 2021, 03:31 a.m.'},
+
+    ]
+
+    comps = [cats, reviews]
+
+    filename = 'population_data'
+    outfile = open(filename,'wb')
+    pickle.dump(comps,outfile)
+    outfile.close()
+    infile = open(filename, 'rb')
+    new_dict = pickle.load(infile)
+    infile.close()
+
+    cats = new_dict[0]
+    reviews = new_dict[1]
+
     # The code below goes through the cats dictionary, then adds each category,
     # and then adds all the associated pages for that category.
 
     for cat, cat_data in cats.items():
         c = add_cat(cat)
         for p in cat_data['pages']:
-            add_product(c, p['name'], p['url'], p['price'], p['description'])
+            add_product(c, p['name'],  p['price'], p['description'])
 
     # Print out the categories we have added.
     for c in Category.objects.all():
         for p in Product.objects.filter(category=c):
             print(f'- {c}: {p}')
 
+    for review in reviews:
+        add_review(review['user'], review['product'], review['rating'], review['content'], review['date'] )
+
     add_review("jeff08", "Asus 11", 8, "Very good!", "Aug. 1, 2021, 7:41 p.m.")
     add_review("jeff04", "Asus 2", 2, "Very bad!", "Jul. 21, 2021, 11:01 p.m.")
 
 
-def add_product(cat, name, url, price, description):
+def add_product(cat, name, price, description):
     p = Product.objects.get_or_create(category=cat, name=name)[0]
-    p.url = url
-
     p.price = price
     p.description = description
-
     p.save()
     return p
 
