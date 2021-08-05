298d252573241cbb4441272092c601e45bc86787
ramon <2593406m@student.gla.ac.uk>
2021-08-05 19:39:53 +0100

    Added last test, took away all "howdy user" text from rango

--- tango_with_django_project/rango/tests.py
+++ tango_with_django_project/rango/tests.py
@@ -1,17 +1,22 @@
 from django.test import TestCase
 
 from rango.models import Category, Product, Store
+from django.urls import reverse
+from django.shortcuts import render, redirect
 
 
 class TestsFunctionality(TestCase):
-    '''Test ensures product slugs are set correctly, this is important
-     as these are used to display static images and link to product page'''
+
+    # test 1
     def test_slug_line_creation(self):
+        '''Test ensures product slugs are set correctly, this is important
+             as these are used to display static images and link to product page'''
         category = Category(name='Len 0')
         category.save()
 
         self.assertEqual(category.slug, 'len-0')
 
+    # test 2
     def test_product_links_store_correctly(self):
         '''Since there is quite a large interdependency between stores and product,
         this test checks that these are correctly linked up'''
@@ -20,6 +25,7 @@ class TestsFunctionality(TestCase):
         product = Product(name='Len', category=category, price=250.32, description='A good one', store=store)
         self.assertEqual('store', str(product.store.name))
 
+    # test 3
     def test_store_lat_lon(self):
         '''This test ensures that no rounding error occurs when inputting
         the longitude and latitude as its extremely important that these are
@@ -30,3 +36,18 @@ class TestsFunctionality(TestCase):
         lat_correct = (lat == store.latitude)
         lon_correct = (lon == store.longitude)
         self.assertTrue(lat_correct and lon_correct)
+
+class IndexViewTests(TestCase):
+    def test_categories_added_correctly(self):
+        '''
+        Test that categories are correctly saved and displayed in the home page
+        '''
+        cat_name = 'category_test'
+        category = Category(name=cat_name)
+        category.save()
+        response = self.client.get(reverse('rango:index'))
+        self.assertContains(response, cat_name)
+
+
+
+
