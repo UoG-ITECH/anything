d314cc03f44325c52cbb9c2741ed3d90b8d08c9d
Jose Mendoza <sagaka1g@gmail.com>
2021-08-05 18:28:25 +0100

    Revert "Fixed some styles and added message pop-up"
    This reverts commit 64c45a2ae50ee60d07c0009d7fbcef7a37598d08.

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -161,15 +161,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Google Maps
 
 GOOGLE_MAPS_API_KEY = 'AIzaSyC4lfCLsP3W3-8TwRTVdpp9Gmw43asTWWE'
-
-# Messages alerts
-
-from django.contrib.messages import constants as messages
-
-MESSAGE_TAGS = {
-    messages.DEBUG: 'alert-info',
-    messages.INFO: 'alert-info',
-    messages.SUCCESS: 'alert-success',
-    messages.WARNING: 'alert-warning',
-    messages.ERROR: 'alert-danger',
-}
