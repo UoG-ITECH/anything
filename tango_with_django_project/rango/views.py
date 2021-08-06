5aff39d0e87823eb5faa668ab8001a8be26739b8
ramon <2593406m@student.gla.ac.uk>
2021-08-06 11:23:40 +0100

    Deleted old unused files

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -25,7 +25,7 @@ class IndexView(View):
         context_dict['categories'] = category_list
         context_dict['products'] = product_list
 
-        visitor_cookie_handler(request)
+
 
         response = render(request, 'rango/index.html', context=context_dict)
         return response
@@ -34,7 +34,7 @@ class IndexView(View):
 class AboutView(View):
     def get(self, request):
         context_dict = {}
-        visitor_cookie_handler(request)
+
         context_dict['visits'] = request.session['visits']
 
         return render(request, 'rango/about.html', context=context_dict)
@@ -305,39 +305,12 @@ class AddReviewView(View):
         return render(request, 'rango/review.html', context=context_dict)
 
 
-@login_required
-def restricted(request):
-    return render(request, 'rango/restricted.html')
 
 
 @login_required
 def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
-
-
-def get_server_side_cookie(request, cookie, default_val=None):
-    val = request.session.get(cookie)
-    if not val:
-        val = default_val
-    return val
-
-
-def visitor_cookie_handler(request):
-    visits = int(get_server_side_cookie(request, 'visits', '1'))
-    last_visit_cookie = get_server_side_cookie(request,
-                                               'last_visit',
-                                               str(datetime.now()))
-    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
-                                        '%Y-%m-%d %H:%M:%S')
-
-    if (datetime.now() - last_visit_time).days > 0:
-        visits = visits + 1
-        request.session['last_visit'] = str(datetime.now())
-    else:
-        request.session['last_visit'] = last_visit_cookie
-
-    request.session['visits'] = visits
 
 
 @login_required
