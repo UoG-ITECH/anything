d64d300a5db5fea28655493068832745eb3df88f
ramon <2593406m@student.gla.ac.uk>
2021-08-06 11:23:40 +0100

    Deleted old unused files

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -9,15 +9,12 @@ urlpatterns = [
     path('category/<slug:category_name_slug>/', views.ShowCategoryView.as_view(), name='show_category'),
     path('category/<slug:category_name_slug>/add_product/', views.AddProductView.as_view(), name='add_product'),
     path('add_category/', views.AddCategoryView.as_view(), name='add_category'),
-    path('restricted/', views.restricted, name='restricted'),
-
     path('search/', views.search, name='search'),
     path('product/<slug>/', views.ShowProductView.as_view(), name='show_computer'),
     path('product/<slug>/review/', views.AddReviewView.as_view(), name='review'),
     path('register_profile/', views.register_profile, name='register_profile'),
     path('register/', views.register, name='register'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
-
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('add_article/', views.add_article, name="add_article"),
